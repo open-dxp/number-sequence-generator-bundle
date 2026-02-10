65d92371b1359a8689ebdbd314384780f68e8e20
open-dxp-stack <stack@opendxp.ch>
2026-02-10 14:19:23 +0100

    Update .php-cs-fixer-finder.dist.php

--- .php-cs-fixer-finder.dist.php
+++ .php-cs-fixer-finder.dist.php
@@ -3,9 +3,4 @@
 return PhpCsFixer\Finder::create()
     ->in([
         __DIR__ . '/src',
-        __DIR__ . '/tests',
-    ])
-    ->exclude([
-        __DIR__ . '/tests/_output',
-        __DIR__ . '/tests/Support/_generated',
     ]);
